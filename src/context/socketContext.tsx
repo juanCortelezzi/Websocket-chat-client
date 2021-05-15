aecd95223d713360ed6eb902d276fb8d982a22ff
juancortelezzi <juanbicortelezzi@gmail.com>
2021-05-15 19:40:06 -0300

    hardcoded ws server link to socketContext

--- src/context/socketContext.tsx
+++ src/context/socketContext.tsx
@@ -2,7 +2,12 @@ import { createContext, useEffect, useState, useContext } from "react";
 import { io, Socket } from "socket.io-client";
 
 const SocketContext = createContext<Socket>(null);
-const endpoint = process.env.ENDPOINT || "http://localhost:3001";
+
+const endpoint =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3001"
+    : "https://socket-encrypted-chat.herokuapp.com/";
+console.log(endpoint);
 
 function SocketProvider({ children }): JSX.Element {
   const [socket, setSocket] = useState<Socket>();
