dd6e1facf109d7f31f9ef483a7057938e05e1ca1
juancortelezzi <juanbicortelezzi@gmail.com>
2021-05-15 19:28:45 -0300

    added fallback ws endpoint on socketcontext

--- src/context/socketContext.tsx
+++ src/context/socketContext.tsx
@@ -2,16 +2,13 @@ import { createContext, useEffect, useState, useContext } from "react";
 import { io, Socket } from "socket.io-client";
 
 const SocketContext = createContext<Socket>(null);
+const endpoint = process.env.ENDPOINT || "http://localhost:3001";
 
 function SocketProvider({ children }): JSX.Element {
-  const endpoint = process.env.ENDPOINT;
-
   const [socket, setSocket] = useState<Socket>();
 
   useEffect((): (() => void) => {
-    const socketio: Socket = io(endpoint, {
-      transports: ["websocket", "polling"],
-    });
+    const socketio: Socket = io(endpoint);
     setSocket(socketio);
     return (): void => {
       socketio.close();
