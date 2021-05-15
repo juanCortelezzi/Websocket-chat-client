bcca30f2004313a1377b659858b85aeaed136911
juancortelezzi <juanbicortelezzi@gmail.com>
2021-05-15 20:02:02 -0300

    moved chakra config from _document to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,22 @@
 import { AppProps } from "next/app";
-import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import {
   UsersProvider,
   SocketProvider,
   MainProvider,
 } from "@/context/contexts";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  config: { initialColorMode: "dark", useSystemColorMode: false },
+});
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <SocketProvider>
       <UsersProvider>
         <MainProvider>
-          <ChakraProvider>
-            <CSSReset />
+          <ChakraProvider theme={theme}>
             <Component {...pageProps} />
           </ChakraProvider>
         </MainProvider>
